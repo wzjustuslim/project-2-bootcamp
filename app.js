71b40fc63df9211d4e00d036459ee98e93660106
wzjustuslim <weizhengjustus.lim@gmail.com>
2021-11-27 16:01:00 +0800

    commit again

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const PORT = 3004;
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 ///
 app.get('/landing', (req, res) => {
